98b9e693463063daa718c68daba5b6fc67d7c7fc
Chansok Oeun <chansok.oeun@camsolution.biz>
2025-01-19 12:50:50 +0700

    suppliers_table

--- app/Models/supplier.php
+++ app/Models/supplier.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class supplier extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'address',
+        'cperson',
+        'contact',
+        'status',
+    ];
 }
