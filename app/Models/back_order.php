aec5d1f1a148ffb637e665dca2fbd3970c539fe3
THEAVY VUN <vun.theavy2003@gmail.com>
2025-01-19 12:50:05 +0700

    add fields of back_order

---
+++ app/Models/back_order.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BackOrder extends Model
+{
+    use HasFactory;
+
+    protected $table = 'back_order';
+
+    protected $fillable = [
+        'receiving_id',
+        'po_id',
+        'supplier_id',
+        'bo_code',
+        'amount',
+        'discount_perc',
+        'discount',
+        'tax_perc',
+        'tax',
+        'remarks',
+        'status',
+    ];
+
+    // Define relationships
+    public function receiving()
+    {
+        return $this->belongsTo(Receiving::class);
+    }
+
+    public function purchaseOrder()
+    {
+        return $this->belongsTo(PurchaseOrder::class, 'po_id');
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+}
---
+++ database/migrations/2025_01_19_044629_create_back_order_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('back_order', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('receiving_id')->constrained();
+            $table->foreignId('po_id')->constrained();
+            $table->foreignId('supplier_id')->constrained();
+            $table->string('bo_code');
+            $table->decimal('amount', 15, 2);
+            $table->decimal('discount_perc', 5, 2);
+            $table->decimal('discount', 15, 2);
+            $table->decimal('tax_perc', 5, 2);
+            $table->decimal('tax', 15, 2);
+            $table->text('remarks')->nullable();
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('back_order');
+    }
+};
