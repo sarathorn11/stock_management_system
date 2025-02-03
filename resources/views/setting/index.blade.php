7f0969d9eb21e58df9017202aafdd573fd4fe815
Lyden CHAI <lyden.chai@gmail.com>
2025-02-03 13:34:21 +0700

    fix

--- resources/views/setting/index.blade.php
+++ resources/views/setting/index.blade.php
@@ -19,12 +19,12 @@
 
   <!-- Flash Messages -->
   @if(session('success'))
-  <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative my-6" role="alert">
+  <div id="successOrFailedMessage" class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative my-6" role="alert">
     <span>{{ session('success') }}</span>
   </div>
   @endif
   @if(session('error'))
-  <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative my-6" role="alert">
+  <div id="successOrFailedMessage" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative my-6" role="alert">
     <span>{{ session('error') }}</span>
   </div>
   @endif
